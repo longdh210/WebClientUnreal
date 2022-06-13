ee1898f8151956d14ba67d7000a2510398244ea1
longdh210 <longdh210@gmail.com>
2022-06-13 12:36:29 +0700

    [UPDATE] add MetaMask and WalletConnect

--- scripts/walletConnect.js
+++ scripts/walletConnect.js
@@ -1,110 +1,190 @@
 "use strict";
 
-// Import NodeWalletConnect and WalletConnectQRCodeModal from the library =
-const NodeWalletConnect = window.WalletConnect.default;
-const WalletConnectQRCodeModal = window.WalletConnectQRCodeModal.default;
+// Unpkg imports
+const Web3Modal = window.Web3Modal.default;
+const WalletConnectProvider = window.WalletConnectProvider.default;
+const EvmChains = window.evmChains;
+console.log("EvmChains", EvmChains);
 
-let walletConnector = null;
-// Create connector
+// Web3modal instance
+let web3Modal;
+
+// Chosen wallet provider given by the dialog window
+let provider;
+
+// Address of the selected account
+let selectedAccount;
+
+/**
+ * Setup
+ */
 function init() {
-  walletConnector = new NodeWalletConnect(
-    {
-      bridge: "https://bridge.walletconnect.org", // Required
-    },
-    {
-      clientMeta: {
-        description: "WalletConnect NodeJS Client",
-        url: "https://nodejs.org/en/",
-        icons: ["https://nodejs.org/static/images/logo.svg"],
-        name: "WalletConnect",
-      },
-    }
-  );
-  console.log("Init walletConnect", walletConnector);
-  check();
-}
-
-// Check if connected for proper UI handling (when user refresh the page)
-function check() {
-  if (walletConnector.connected) {
-    document.querySelector("#connected").style.display = "block";
-    document.querySelector("#prepare").style.display = "none";
-  }
-}
-
-// Change button when use connected
-async function changeButton() {
-  document.querySelector("#connected").style.display = "block";
-  document.querySelector("#prepare").style.display = "none";
-}
-
-// Check if connection is already established
-async function showQRCode() {
-  if (!walletConnector.connected) {
-    // create new session
-    walletConnector.createSession().then(() => {
-      // get uri for QR Code modal
-      const uri = walletConnector.uri;
-      // display QR Code modal
-      WalletConnectQRCodeModal.open(
-        uri,
-        () => {
-          console.log("QR Code Modal closed");
-        },
-        true // isNode = true
-      );
-    });
-  }
-  // Subscribe to connection events
-  walletConnector.on("connect", async (error, payload) => {
-    if (error) {
-      throw error;
-    }
-
-    // Close QR Code Modal
-    WalletConnectQRCodeModal.close(
-      true // isNode = true
+    console.log("Initializing example");
+    console.log("WalletConnectProvider is", WalletConnectProvider);
+    console.log(
+        "window.web3 is",
+        window.web3,
+        "window.ethereum is",
+        window.ethereum
     );
 
-    // Get provided accounts and chainId
-    const { accounts, chainId } = payload.params[0];
-    await changeButton();
-    console.log("Connected");
-    console.log("Address:", accounts);
-    console.log("ChainId", chainId);
-  });
+    const providerOptions = {
+        walletconnect: {
+            package: WalletConnectProvider,
+            options: {
+                infuraId: "a2cfc47d9a4f408ea304fef5b70e5599",
+            },
+        },
+    };
 
-  walletConnector.on("session_update", (error, payload) => {
-    if (error) {
-      throw error;
-    }
+    web3Modal = new Web3Modal({
+        cacheProvider: false, // optional
+        providerOptions, // required
+    });
 
-    // Get updated accounts and chainId
-    const { accounts, chainId } = payload.params[0];
-    console.log(accounts, chainId);
-  });
+    console.log("Web3Modal instance is", web3Modal);
 }
 
-function disconnect() {
-  walletConnector.on("disconnect", (error, payload) => {
-    if (error) {
-      throw error;
-    }
+/**
+ * Kick in the UI action after Web3modal dialog has chosen a provider
+ */
+async function fetchAccountData() {
+    // Get a Web3 instance for the wallet
+    const web3 = new Web3(provider);
 
-    // Delete walletConnector
-  });
-  walletConnector.killSession().then(() => {
-    console.log("Disconnect successfully");
-  });
-  document.querySelector("#prepare").style.display = "block";
-  document.querySelector("#connected").style.display = "none";
+    console.log("Web3 instance is", web3);
+
+    // Get connected chain id from Ethereum node
+    const chainId = await web3.eth.getChainId();
+    // Load chain information over an HTTP API
+    console.log("Chain data is", EvmChains);
+    const chainData = await EvmChains.getChain(chainId);
+    // document.querySelector("#network-name").textContent = chainData.name;
+    console.log("chain name", chainData.name);
+
+    // Get list of accounts of the connected wallet
+    const accounts = await web3.eth.getAccounts();
+
+    // MetaMask does not give you all accounts, only the selected account
+    console.log("Got accounts", accounts);
+    selectedAccount = accounts[0];
+
+    // document.querySelector("#selected-account").textContent = selectedAccount;
+    console.log("selected account", selectedAccount);
+
+    // Get a handl
+    // const template = document.querySelector("#template-balance");
+    // const accountContainer = document.querySelector("#accounts");
+
+    // // Purge UI elements any previously loaded accounts
+    // accountContainer.innerHTML = "";
+
+    // Go through all accounts and get their ETH balance
+    // const rowResolvers = accounts.map(async (address) => {
+    //     const balance = await web3.eth.getBalance(address);
+    //     // ethBalance is a BigNumber instance
+    //     // https://github.com/indutny/bn.js/
+    //     const ethBalance = web3.utils.fromWei(balance, "ether");
+    //     const humanFriendlyBalance = parseFloat(ethBalance).toFixed(4);
+    //     // Fill in the templated row and put in the document
+    //     const clone = template.content.cloneNode(true);
+    //     clone.querySelector(".address").textContent = address;
+    //     clone.querySelector(".balance").textContent = humanFriendlyBalance;
+    //     accountContainer.appendChild(clone);
+    // });
+
+    // Because rendering account does its own RPC commucation
+    // with Ethereum node, we do not want to display any results
+    // until data for all accounts is loaded
+    // await Promise.all(rowResolvers);
+
+    // Display fully loaded UI for wallet data
+    document.querySelector("#prepare").style.display = "none";
+    document.querySelector("#connected").style.display = "block";
 }
 
-// Catch event when init page and user click button
+/**
+ * Fetch account data for UI when
+ * - User switches accounts in wallet
+ * - User switches networks in wallet
+ * - User connects wallet initially
+ */
+async function refreshAccountData() {
+    // If any current data is displayed when
+    // the user is switching acounts in the wallet
+    // immediate hide this data
+    document.querySelector("#connected").style.display = "none";
+    document.querySelector("#prepare").style.display = "block";
+
+    // Disable button while UI is loading.
+    // fetchAccountData() will take a while as it communicates
+    // with Ethereum node via JSON-RPC and loads chain data
+    // over an API call.
+    document.querySelector("#btn-connect").setAttribute("disabled", "disabled");
+    await fetchAccountData(provider);
+    document.querySelector("#btn-connect").removeAttribute("disabled");
+}
+
+/**
+ * Connect wallet button pressed.
+ */
+async function onConnect() {
+    console.log("Opening a dialog", web3Modal);
+    try {
+        provider = await web3Modal.connect();
+    } catch (e) {
+        console.log("Could not get a wallet connection", e);
+        return;
+    }
+
+    // Subscribe to accounts change
+    provider.on("accountsChanged", (accounts) => {
+        fetchAccountData();
+    });
+
+    // Subscribe to chainId change
+    provider.on("chainChanged", (chainId) => {
+        fetchAccountData();
+    });
+
+    // Subscribe to networkId change
+    provider.on("networkChanged", (networkId) => {
+        fetchAccountData();
+    });
+
+    await refreshAccountData();
+}
+
+/**
+ * Disconnect wallet button pressed.
+ */
+async function onDisconnect() {
+    console.log("Killing the wallet connection", provider);
+
+    // TODO: Which providers have close method?
+    if (provider.close) {
+        await provider.close();
+
+        // If the cached provider is not cleared,
+        // WalletConnect will default to the existing session
+        // and does not allow to re-scan the QR code with a new wallet.
+        await web3Modal.clearCachedProvider();
+        provider = null;
+    }
+
+    selectedAccount = null;
+
+    // Set the UI back to the initial state
+    document.querySelector("#prepare").style.display = "block";
+    document.querySelector("#connected").style.display = "none";
+}
+/**
+ * Main entry point.
+ */
 window.addEventListener("load", async () => {
-  init();
-  document.querySelector("#btn-connect").addEventListener("click", showQRCode);
-  document
-    .querySelector("#btn-disconnect")
-    .addEventListener("click", disconnect);
+    init();
+    document.querySelector("#btn-connect").addEventListener("click", onConnect);
+    document
+        .querySelector("#btn-disconnect")
+        .addEventListener("click", onDisconnect);
 });
